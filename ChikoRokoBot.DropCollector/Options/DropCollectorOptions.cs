0ee2e28f9b4d7594bbd0ee52a87f409bfab9838c
Artem Kiyashko <artem86@hotmail.co.uk>
2023-06-03 00:38:08 +0200

    change base address

--- ChikoRokoBot.DropCollector/Options/DropCollectorOptions.cs
+++ ChikoRokoBot.DropCollector/Options/DropCollectorOptions.cs
@@ -4,7 +4,7 @@ namespace ChikoRokoBot.DropCollector.Options
 {
     public class DropCollectorOptions
     {
-		public Uri Url { get; set; } = new Uri("https://chikoroko.art/");
+		public Uri Url { get; set; } = new Uri("https://chikoroko.art/en");
         public string DataElementQuerySelector { get; set; } = "script#__NEXT_DATA__";
         public string StorageAccount { get; set; } = "UseDevelopmentStorage=true";
         public string QueueName { get; set; } = "alldrops";
